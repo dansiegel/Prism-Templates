97b6ed3f74e122172c4905a393d8ce152fba7b67
Dan Siegel <me@dansiegel.net>
2017-04-24 19:06:59 -0700

    Initial Commit

---
+++ src/PrismTemplate/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PropertyChanged;
+using Xamarin.Forms;
+
+namespace PrismTemplate.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class MainPageViewModel
+    {
+        public string Message { get; set; }
+    }
+}
+
---
+++ src/PrismTemplate/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismTemplate.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
