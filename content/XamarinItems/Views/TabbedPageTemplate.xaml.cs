08b06d395f4b7ced05ce1c4f65b6c35906e3b81b
Dan Siegel <me@dansiegel.net>
2017-07-14 16:14:24 -0700

    updating item templates

--- content/XamarinItems/Views/TabbedPageTemplate.xaml.cs
+++ content/XamarinItems/Views/TabbedPageTemplate.xaml.cs
@@ -1,22 +1,12 @@
-using Prism.Common;
-using Prism.Navigation;
 using Xamarin.Forms;
 
 namespace MobileApp.Views
 {
-    public partial class ItemTemplate : TabbedPage, INavigatingAware
+    public partial class ItemTemplate : PrismTabbedPage
     {
         public ItemTemplate()
         {
             InitializeComponent();
-        }
-
-        public void OnNavigatingTo(NavigationParameters parameters)
-        {
-            foreach(var child in Children)
-            {
-                PageUtilities.OnNavigatingTo(child, parameters);
-            }
         }
     }
 }
