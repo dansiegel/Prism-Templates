06862f15c628ef6777b0b865a2de0b6eceb3b72e
Dan Siegel <me@dansiegel.net>
2017-07-14 12:16:44 -0700

    adding Prism friendly TabPage’s

---
+++ content/MobileAppQuickStart-CSharp/src/Company.MobileApp/Views/DynamicTabbedPage.cs
@@ -0,0 +1,95 @@
+#if (AutofacContainer)
+using Autofac;
+#endif
+#if (DryIocContainer)
+using DryIoc;
+#endif
+#if (NinjectContainer)
+using Ninject;
+#endif
+#if (UnityContainer)
+using Microsoft.Practices.Unity;
+#endif
+using Prism.Common;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Company.MobileApp.Views
+{
+    public class DynamicTabbedPage : TabbedPage, INavigatingAware
+    {
+#if (AutofacContainer || DryIocContainer)
+        private IContainer _container { get; }
+
+        public DynamicTabbedPage(IContainer container)
+#endif
+#if (NinjectContainer)
+        private IKernel _kernel { get; }
+
+        public DynamicTabbedPage(IKernel kernel)
+#endif
+#if (UnityContainer)
+        private IUnityContainer _container { get; }
+
+        public DynamicTabbedPage(IUnityContainer container)
+#endif
+        {
+#if (NinjectContainer)
+            _kernel = kernel;
+#else
+            _container = container;
+#endif
+        }
+
+        public async void OnNavigatingTo(NavigationParameters parameters)
+        {
+            var tabs = parameters.GetValues<string>("tab");
+            foreach(var tabSegment in tabs)
+            {
+                Page page = null;
+
+                foreach(var segment in tabSegment.Split('/'))
+                {
+                    switch(page)
+                    {
+                        case null:
+                            page = CreatePage(segment);
+                            PageUtilities.OnNavigatingTo(page, parameters);
+                            break;
+                        case NavigationPage navPage:
+                            var childPage = CreatePage(segment);
+                            PageUtilities.OnNavigatingTo(childPage, parameters);
+                            await navPage.PushAsync(childPage);
+                            break;
+                    }
+                }
+
+                Children.Add(page);
+            }
+        }
+
+        protected virtual Page CreatePage(string childName)
+        {
+#if (AutofacContainer)
+            if (!_container.IsRegisteredWithName<Page>(childName))
+                throw new NullReferenceException($"The requested page '{childName}' has not been registered.");
+
+            var page = _container.ResolveNamed<Page>(childName);
+#endif
+#if (DryIocContainer || UnityContainer)
+            var page = _container.Resolve<object>(childName) as Page;
+#endif
+#if (NinjectContainer)
+            var page = _kernel.Get<object>(name) as Page;
+#endif
+
+            if(ViewModelLocator.GetAutowireViewModel(page) == null)
+            {
+                ViewModelLocator.SetAutowireViewModel(page, true);
+            }
+
+            return page;
+        }
+    }
+}
---
+++ content/MobileAppQuickStart-CSharp/src/Company.MobileApp/Views/PrismTabbedPage.cs
@@ -0,0 +1,17 @@
+using Prism.Common;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Company.MobileApp.Views
+{
+    public class PrismTabbedPage : TabbedPage, INavigatingAware
+    {
+        public virtual void OnNavigatingTo(NavigationParameters parameters)
+        {
+            foreach(var child in Children)
+            {
+                PageUtilities.OnNavigatingTo(child, parameters);
+            }
+        }
+    }
+}
