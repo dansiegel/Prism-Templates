5b380e742505b24b6d05fe4273854f19de227ed0
Dan Siegel <me@dansiegel.net>
2017-07-13 17:28:28 -0700

    fixing formatting

--- content/MobileAppQuickStart-CSharp/BuildHostSecrets.ps1
+++ content/MobileAppQuickStart-CSharp/BuildHostSecrets.ps1
@@ -16,37 +16,53 @@ $secretNames = @(
 #endif
 )
 
-function New-Json {
-[CmdletBinding()]
-param([Parameter(ValueFromPipeline=$true)][HashTable]$InputObject) 
-begin { 
-   $ser = @{}
-   $jsona = @()
-}
-process {
-   $jsoni = 
-   foreach($input in $InputObject.GetEnumerator() | Where { $_.Value } ) {
-      if($input.Value -is [Hashtable]) {
-         '"'+$input.Key+'": ' + (New-JSon $input.Value)
-      } else {
-         $type = $input.Value.GetType()
-         if(!$Ser.ContainsKey($Type)) {
-            $Ser.($Type) = New-Object System.Runtime.Serialization.Json.DataContractJsonSerializer $type
-         }
-         $stream = New-Object System.IO.MemoryStream
-         $Ser.($Type).WriteObject( $stream, $Input.Value )
-         '"'+$input.Key+'": ' + (Read-Stream $stream)
+function New-Json 
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(ValueFromPipeline=$true)][HashTable]$InputObject
+  )
+
+  begin
+  {
+    $ser = @{}
+    $jsona = @()
+  }
+  process
+  {
+    $jsoni = 
+    foreach($input in $InputObject.GetEnumerator() | Where { $_.Value } ) 
+    {
+      if($input.Value -is [Hashtable]) 
+      {
+        '"'+$input.Key+'": ' + (New-JSon $input.Value)
       }
-   }
+      else
+      {
+        $type = $input.Value.GetType()
+        if(!$Ser.ContainsKey($Type)) 
+        {
+          $Ser.($Type) = New-Object System.Runtime.Serialization.Json.DataContractJsonSerializer $type
+        }
+        $stream = New-Object System.IO.MemoryStream
+        $Ser.($Type).WriteObject( $stream, $Input.Value )
+        '"'+$input.Key+'": ' + (Read-Stream $stream)
+      }
+    }
 
-   $jsona += "{`n" +($jsoni -join ",`n")+ "`n}"
-}
-end { 
-   if($jsona.Count -gt 1) {
-      "[$($jsona -join ",`n")]" 
-   } else {
+    $jsona += "{`n" +($jsoni -join ",`n")+ "`n}"
+  }
+  end
+  {
+    if($jsona.Count -gt 1)
+    {
+      "[$($jsona -join ",`n")]"
+    } 
+    else 
+    {
       $jsona
-   }
-}}
+    }
+  }
+}
 
-Out-File -FilePath ./src/MobileApp/secrets.json -Encoding ASCII -Force -InputObject "{$json\n}"
+Out-File -FilePath ./src/Company.MobileApp/secrets.json -Encoding ASCII -Force -InputObject "{$json\n}"
