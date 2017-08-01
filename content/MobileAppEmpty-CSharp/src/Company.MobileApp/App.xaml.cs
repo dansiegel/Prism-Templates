1d9395fc8e6fb2d7fe088fb68534896564c034a6
Dan Siegel <me@dansiegel.net>
2017-08-01 12:06:23 -0700

    Xaml Previewer compatibility

--- content/MobileAppEmpty-CSharp/src/Company.MobileApp/App.xaml.cs
+++ content/MobileAppEmpty-CSharp/src/Company.MobileApp/App.xaml.cs
@@ -26,7 +26,18 @@ namespace Company.MobileApp
 {
     public partial class App : PrismApplication
     {
-        public App(IPlatformInitializer initializer = null)
+        /* 
+         * NOTE: 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() 
+            : this(null)
+        {
+        }
+
+        public App(IPlatformInitializer initializer)
             : base(initializer)
         {
         }
